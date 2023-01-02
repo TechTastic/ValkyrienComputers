e62a0d420fe9ae9720e1e72e8911bf98d5a75ea1
walksanatora <walkerffo22@gmail.com>
2023-01-02 01:00:45 -0700

    pull commit 91c1d4c54d689b6e850f1bae62a8ba02f988186e from 1.16

--- fabric/src/main/java/net/techtastic/vc/fabric/integrations/cc/valkyrienskies/RadarPeripheral.java
+++ fabric/src/main/java/net/techtastic/vc/fabric/integrations/cc/valkyrienskies/RadarPeripheral.java
@@ -7,7 +7,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
 import net.techtastic.vc.ValkyrienComputersConfig;
-import net.techtastic.vc.ValkyrienComputersBlocksCC;
+import net.techtastic.vc.cc.ValkyrienComputersBlocksCC;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Quaterniondc;
